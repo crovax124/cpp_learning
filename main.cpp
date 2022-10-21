86410cd17ba6a0306e23c481f3dd6981903999b4
crovax124 <eric.isensee11@gmail.com>
2022-10-21 14:37:44 +0100

    first litte syntax practice

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ public:
     static consteval int get_value(int value){
         return value;
     }
+    static void printNameTenTimes(){
+        string name;
+        cout << "Type in Name:" <<endl;
+        cin >> name ;
+
+        for(int i=0; i<10;i++){
+            std::cout<< to_string(i+1) + "."+ get_name(name)<<endl;
+        }
+    }
+private:
     static string get_name(string value){
         return value;
     }
@@ -13,11 +23,6 @@ public:
 
 
 int main() {
-    string name;
-    cout << "Type in Name:" <<endl;
-    cin >> name ;
-    for(int i=0; i<10;i++){
-       std::cout<< to_string(i+1) + "."+ MyNumberRepo::get_name(name)<<endl;
-    }
+MyNumberRepo::printNameTenTimes();
     return 0;
 }
